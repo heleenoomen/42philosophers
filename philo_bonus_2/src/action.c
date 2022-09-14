beb137fb56bb5c7c6686660b5b8fc6939067c5e6
Heleen Oomen <hoomen@student.42heilbronn.de>
2022-09-14 14:04:07 +0200

    bonus problem found: post on check semaphores before exiting when sated

--- philo_bonus_2/src/action.c
+++ philo_bonus_2/src/action.c
@@ -6,7 +6,7 @@
 /*   By: hoomen <hoomen@student.42heilbronn.de      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 11:38:50 by hoomen            #+#    #+#             */
-/*   Updated: 2022/09/14 12:00:13 by hoomen           ###   ########.fr       */
+/*   Updated: 2022/09/14 12:27:09 by hoomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,10 @@ void	ph_eat(t_ctrl *ctrl)
 	ph_usleep(ctrl, ctrl->time_eat - (gettime() - ctrl->last_meal));	
 	sem_post(ctrl->forks);
 	sem_post(ctrl->forks);
-	set_status(ctrl, NOT_EATING);
+	printf("%i is here (38)\n", ctrl->index);
+//	set_status(ctrl, NOT_EATING);
 	ctrl->meals++;
+	printf("%i is here\n", ctrl->index);
 	if (ctrl->meals == ctrl->max_meals)
 	{
 		pthread_detach(ctrl->watcher);
